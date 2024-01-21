6bf0d9616dfc80c0734fbb7f1cdc47ffe3d174f4
Gleor <37976931+Gleor@users.noreply.github.com>
2024-01-21 13:48:28 +0300

    Update log_duration.h

--- search-server/log_duration.h
+++ search-server/log_duration.h
@@ -12,8 +12,6 @@
 
 class LogDuration {
 public:
-    // заменим имя типа std::chrono::steady_clock
-    // с помощью using для удобства
     using Clock = std::chrono::steady_clock;
 
     LogDuration(std::string_view id, std::ostream& dst_stream = std::cerr)
@@ -35,18 +33,3 @@ private:
     const Clock::time_point start_time_ = Clock::now();
     std::ostream& dst_stream_;
 };
-
-/*
-Correct function MergeSort in this code.It must slill use async computations. You can not use method wait() and other sort methods in MergeSort. Write improved code
-template void MergeSort(RandomIt range_begin, RandomIt range_end) {
-    int range_length = range_end - range_begin;
-    if (range_length < 2) { return; }
-    vector elements(range_begin, range_end);
-    auto mid = elements.begin() + range_length / 2;
-    auto future1 = async([&elements, &mid]() { MergeSort(elements.begin(), mid); });
-    auto future2 = async([&elements, &mid]() { MergeSort(mid, elements.end()); });
-    future1.wait();
-    future2.wait();
-    merge(elements.begin(), mid, mid, elements.end(), range_begin);
-}
-*/
